48a270261d493d240eb1c74c1a53e379a8b18ac5
marion001 <27297851+marion001@users.noreply.github.com>
2024-09-14 17:10:56 +0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # VBot_Offline
 Trợ lý ảo VBot
+
+Không hỗ trợ với: Raspberrypi Zero 1 W
+Chỉ hỗ trợ từ: Raspberrypi Zero 2 W (trở lên)
---
+++ Start.py
@@ -0,0 +1,9 @@
+'''
+Code By: Vũ Tuyển
+Designed by: BootstrapMade
+Facebook: https://www.facebook.com/TWFyaW9uMDAx
+'''
+import VBot
+
+if __name__ == "__main__":
+    VBot.main()
