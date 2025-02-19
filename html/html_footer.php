b83c678e98bd4b001dcb824a9089fe8d5c95cb07
marion001 <mr.marion001@gmail.com>
2025-02-19 22:00:08 +0700

    first commit

--- html/html_footer.php
+++ html/html_footer.php
@@ -14,6 +14,6 @@ Facebook: https://www.facebook.com/TWFyaW9uMDAx
       <!-- You can delete the links only if you purchased the pro version. -->
       <!-- Licensing information: https://bootstrapmade.com/license/ -->
       <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-      Code by <a href="https://www.facebook.com/TWFyaW9uMDAx" target="_bank">(<i class="bi bi-facebook"></i> Vũ Tuyển)</a> <a href="https://github.com/marion001/VBot_Offline" target="_bank"><i class="bi bi-github"></i> GitHub</a>  <a href="https://drive.google.com/drive/folders/1rB3P8rev2byxgRsXS7mAdkKRj7j0M4xZ" target="_bank"><i class="bi bi-folder2-open"></i> VBot IMG</a>, Designed by: <a href="https://bootstrapmade.com/" target="_bank">BootstrapMade</a>
+      Code by <a href="https://www.facebook.com/TWFyaW9uMDAx" target="_bank">(<i class="bi bi-facebook"></i> Vũ Tuyển)</a>, <a href="https://www.facebook.com/groups/1148385343358824" target="_bank"><i class="bi bi-facebook"></i> Group Facebook</a>, <a href="https://github.com/marion001/VBot_Offline" target="_bank"><i class="bi bi-github"></i> GitHub</a>,  <a href="https://drive.google.com/drive/folders/1rB3P8rev2byxgRsXS7mAdkKRj7j0M4xZ" target="_bank"><i class="bi bi-folder2-open"></i> VBot IMG</a>, Designed by: <a href="https://bootstrapmade.com/" target="_bank">BootstrapMade</a>
     </div>
   </footer>
